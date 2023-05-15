cc7d479cee61faa01ad197703216e6321a70e006
Viktor <vmakksimov@gmail.com>
2023-05-15 16:57:03 +0300

    Home page buttons

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -3,25 +3,28 @@ import animals from '../../styles/images/kindpng_2140500.png'
 import paw from '../../styles/images/paw.png'
 
 
-export const Home = ({pets}) => {
+export const Home = ({ pets }) => {
 
     const pet = pets.find(x => x.name == 'Aya' && 'true')
     console.log(pet)
 
     return (
-        <div className="home-content">
-            <section className='title-content'>
-                <h2><strong>Day Care</strong></h2>
-                <img src={paw} alt="" />
-            </section>
-            <section className='image-content'>
-                {/* {pet !== undefined && <img src={pet.image} alt="Aya" ></img>} */}
-               <h1>Animals</h1>
-               <img src={animals} alt="" />
-                
-            </section>
-            
-            
-        </div>
+        <article className='daycare-content'>
+            <div className="home-content">
+                <section className='title-content'>
+                    <section className='title-buttons'>
+                        <h1>Day Care</h1>
+                        <h2>Your <strong>Pet</strong> is Part of Our Family</h2>
+                        <button className='learn'>Learn More</button>
+                        <button className='reservation'>Make a Reservation</button>
+                    </section>
+                    <img src={paw} alt="" />
+                </section>
+                <section className='image-content'>
+                    <img src={animals} alt="animals" />
+                </section>
+            </div>
+
+        </article>
     )
 }
