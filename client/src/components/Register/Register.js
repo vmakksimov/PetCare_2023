8e9693659958ae8b4d163acb5cafc6cbca2de041
Viktor <vmakksimov@gmail.com>
2023-05-21 05:42:42 +0300

    Adding Registration form

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,3 +1,5 @@
+import './Register.css'
+
 export const Register = () => {
     return (
         <section className="daycare-registration">
@@ -5,10 +7,30 @@ export const Register = () => {
             <article className="daycare-form">
                 <form action="#">
                     <div className="user-details">
-                        <span className="user">Username</span>
-                        <input type="text" name="username"></input>
+                        <input type="text" name="username" placeholder="Username"></input>
                     </div>
-                    <div className="form-button">Button</div>
+                    <div className="user-details">
+                        <input type="text" name="email" placeholder="Email"></input>
+                    </div>
+                    <div className="user-details">
+                        <input type="text" name="first_name" placeholder="First Name"></input>
+                    </div>
+                    <div className="user-details">
+                        <input type="text" name="last_name" placeholder="Last Name"></input>
+                    </div>
+                    <div className="user-details">
+                        <input type="password" name="password" placeholder="Password"></input>
+                    </div>
+                    <div className="user-details">
+                        <input type="password" name="re_password" placeholder="Confirm Password"></input>
+                    </div>
+                    <div className="confirm-form">
+                        <input type="checkbox" />
+                        <p>I agree with T&C</p>
+                    </div>
+                    <div className="form-button">
+                        <input type="submit" value="Register" />
+                    </div>
                 </form>
             </article>
 
