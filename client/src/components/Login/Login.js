e1fe27c103a87c416df1c659b92392d60fadd1d8
Viktor <vmakksimov@gmail.com>
2023-05-27 17:00:24 +0300

    login page

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,28 +1,34 @@
 import './Login.css'
 import { Link } from 'react-router-dom'
+import login from '../../styles/images/login.png'
+import friends from '../../styles/images/friends7.jpg'
 
 
 export const Login = () => {
     return (
-        <article className="login-page">
-            <section className="form-title"><h1>SIGN IN</h1></section>
-            <section className="login-page-details">
-                <form className="login-form" method="POST">
-                    <div className="user-details">
-                        <input type='text' name="username" placeholder='username'></input>
-                    </div>
-                    <div className="user-details">
-                        <input type='password' name="password" placeholder='password'></input>
-                    </div>
-                    <div className="user-details">
-                        <input type='password' name="re_password" placeholder='confirm password'></input>
-                    </div>
-                    <div className="form-button">
+        <article className='login-page-image'>
+            <img src={friends} alt="" className='background-image-login' />
+            <article className="login-page">
+                <img src={login} alt="" />
+                <section className="form-title"><h1>SIGN IN</h1></section>
+                <section className="login-page-details">
+                    <form className="login-form" method="POST">
+                        <div className="user-details">
+                            <input type='text' name="username" placeholder='username'></input>
+                        </div>
+                        <div className="user-details">
+                            <input type='password' name="password" placeholder='password'></input>
+                        </div>
+                        <div className="user-details">
+                            <input type='password' name="re_password" placeholder='confirm password'></input>
+                        </div>
+                        <div className="form-button">
                             <input type="submit" value="SIGN IN" />
-                    </div>
-                    <h3 className='account-h3'>Don't have an account? <Link>SIGN UP here</Link></h3>
-                </form>
-            </section>
+                        </div>
+                        <h3 className='account-h3'>Don't have an account? <Link>SIGN UP here</Link></h3>
+                    </form>
+                </section>
+            </article>
         </article>
     )
 }
