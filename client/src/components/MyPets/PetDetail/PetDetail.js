33f64824e8677a0e8f368ee2b0d3ea7190bec1fe
Viktor <vmakksimov@gmail.com>
2023-07-05 05:37:35 +0300

    Adding Css to Pet Details

--- client/src/components/MyPets/PetDetail/PetDetail.js
+++ client/src/components/MyPets/PetDetail/PetDetail.js
@@ -2,14 +2,17 @@ export const PetDetail = ({ pet }) => {
     return (
         <>
             <div className="personal-pet-image">
-                <img src={pet.image} alt="" />
+                <div className="pet-image">
+                    <img src={pet.image} alt="" />
+                </div>
+                <div className="personal-pet-details">
+                    <p>Name: {pet.name} </p>
+                    <p>Age: {pet.age}</p>
+                    <p>Kind: {pet.kind} </p>
+                    <p>Gender: {pet.gender}</p>
+                </div>
             </div>
-            <div className="personal-pet-details">
-                <p>Name: {pet.name} </p>
-                <p>Age: {pet.age}</p>
-                <p>Kind: {pet.kind} </p>
-                <p>Gender: {pet.gender}</p>
-            </div>
+
         </>
     )
 }
