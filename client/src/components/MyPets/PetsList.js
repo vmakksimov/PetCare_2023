d71ff5402e58821d483c790ec416f48752fdc5c9
Viktor <vmakksimov@gmail.com>
2023-07-23 16:26:36 +0300

    Applying css styles on pets details

--- client/src/components/MyPets/PetsList.js
+++ client/src/components/MyPets/PetsList.js
@@ -3,21 +3,39 @@ import { AuthContext } from "../context/AuthContext"
 import { PetsContext } from "../context/PetsContext";
 import './MyPets.css'
 import { PetDetail } from "./PetDetail/PetDetail";
+import paws from '../../styles/images/paws.png'
+import paw from '../../styles/images/paw.png'
+import mypets from '../../styles/images/mypets.png'
+
 export const MyPets = () => {
 
     const { pets } = useContext(PetsContext);
     const { user } = useContext(AuthContext)
-    
+
     console.log(pets)
     const ownerPets = pets.filter(x => x.owner == user._id)
- 
+
 
     return (
-        <div className="personal-pet-section">
-            {ownerPets.length > 0
-                ? ownerPets.map(x => <PetDetail key={x.id} pet={x} />)
-                : <span>This user has no pets yet.</span>
-            }
-        </div>
+        <article className='daycare-header'>
+            <div className="home-content">
+                <section className='title-content'>
+                    <section className='title-buttons'>
+                        <h1>My Pets</h1>
+                    </section>
+                    <img src={paws} alt="" />
+                </section>
+                <section className='image-content'>
+                    <img src={mypets} alt="animals" />
+                </section>
+            </div>
+
+            <div className="personal-pet-section">
+                {ownerPets.length > 0
+                    ? ownerPets.map(x => <PetDetail key={x.id} pet={x} />)
+                    : <span>This user has no pets yet.</span>
+                }
+            </div>
+        </article>
     )
 }
